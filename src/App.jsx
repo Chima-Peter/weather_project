ab1846698ed9f94ae5478c6f60cae812f53e1b3e
Mobi <chimaojimma@gmail.com>
2024-07-15 17:42:32 +0100

    Made initial necessary settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./components/pages/home"
+import ErrorPage from "./components/pages/errorpage"
+
 function App() {
    return (
       <BrowserRouter
-      basename={import.meta.env.DEV ? '/' : '/repo_name/'}
+      basename={import.meta.env.DEV ? '/' : '/weather_project/'}
       >
          <Routes>
-            <Route path="" element="" />
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<ErrorPage />} />
          </Routes>
       </BrowserRouter>
    )
