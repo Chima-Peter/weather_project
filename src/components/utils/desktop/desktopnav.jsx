2e1a50ac5777063be5b94ca6cbeb972540ef830d
Mobi <chimaojimma@gmail.com>
2024-07-20 01:47:34 +0100

    Add region data

--- src/components/utils/desktop/desktopnav.jsx
+++ src/components/utils/desktop/desktopnav.jsx
@@ -1,4 +1,5 @@
 import { FaLocationCrosshairs } from "react-icons/fa6";
+import { CiLocationOn } from "react-icons/ci";
 import { useContext, useEffect, useRef } from "react";
 import { FaGlobeAmericas } from "react-icons/fa";
 import { FaSearch } from "react-icons/fa";
@@ -36,53 +37,57 @@ function DesktopNav() {
 
    return (
    <nav className="z-20 flex justify-between  z-90 top-0">
-      {
-         active && <>
-            <div className="flex gap-2 items-center">
-               <FaLocationCrosshairs className="text-white w-4 h-4"/>
-               <h1 className="font-medium text-sm text-white">
-                  {fetchData.location.name}
-               </h1>
-            </div>
-            <form noValidate onSubmit={handleSearch} className="">
-               <label htmlFor="search" className="border-b-2 border-b-white flex text-white">
-                  <input 
-                     ref={inputRef}
-                     type="text" 
-                     name="search" 
-                     id="search" 
-                     autoFocus 
-                     value={search}
-                     autoComplete="off"
-                     placeholder="Search your city name...."
-                     onChange={searchChange}
-                     className="bg-inherit text-center pb-2 placeholder:text-xs placeholder:text-white placeholder:font-semibold placeholder:normal-case capitalize focus:outline-none font-semibold text-white text-sm w-100 border-0" />
-                  <button type="submit">
-                     <FaSearch className="h-5 text-white w-5 cursor-pointer"/>
-                  </button>
-               </label>
-            </form>
-            <div className="flex gap-8 items-center">
-               <div className="flex gap-2 items-center">
-                  <FaGlobeAmericas className="text-white w-4 h-4"/>
-                  <h1 className="font-medium text-sm text-white">
-                     {fetchData.location.country}
-                  </h1>
-               </div>
-               {
-                  deg && <h1 className="text-sm font-bold text-white cursor-pointer" onClick={() => setDeg(!deg)}>
-                     &deg;C
-                  </h1>
-               }
-               {
-                  !deg && <h1 className="text-sm font-bold text-white cursor-pointer" onClick={() => setDeg(!deg)}>
-                     &deg;F
-                  </h1>
-               }
-            </div>
-            
-         </>
-      }
+      <div className="flex gap-8 items-center">
+         <div className="flex gap-2 items-center">
+            <FaLocationCrosshairs className="text-white w-4 h-4"/>
+            <h1 className="font-medium md:text-xs lg:text-sm text-white">
+               {fetchData.location.name}
+            </h1>
+         </div>
+         <div className="flex gap-2 items-center">
+            <CiLocationOn
+                className="text-white w-4 h-4"/>
+            <h1 className="font-medium md:text-xs lg:text-sm text-white">
+               {fetchData.location.region}
+            </h1>
+         </div>
+      </div>
+      <form noValidate onSubmit={handleSearch} className="">
+         <label htmlFor="search" className="border-b-2 border-b-white flex text-white">
+            <input 
+               ref={inputRef}
+               type="text" 
+               name="search" 
+               id="search" 
+               autoFocus 
+               value={search}
+               autoComplete="off"
+               placeholder="Search your city name...."
+               onChange={searchChange}
+               className="bg-inherit text-center pb-2 placeholder:text-xs placeholder:text-white placeholder:font-semibold placeholder:normal-case capitalize focus:outline-none font-semibold text-white text-sm w-100 border-0" />
+            <button type="submit">
+               <FaSearch className="h-5 text-white w-5 cursor-pointer"/>
+            </button>
+         </label>
+      </form>
+      <div className="flex gap-8 items-center">
+         <div className="flex gap-2 items-center">
+            <FaGlobeAmericas className="text-white w-4 h-4"/>
+            <h1 className="font-medium md:text-xs lg:text-sm text-white">
+               {fetchData.location.country}
+            </h1>
+         </div>
+         {
+            deg && <h1 className="text-sm font-bold text-white cursor-pointer" onClick={() => setDeg(!deg)}>
+               &deg;C
+            </h1>
+         }
+         {
+            !deg && <h1 className="text-sm font-bold text-white cursor-pointer" onClick={() => setDeg(!deg)}>
+               &deg;F
+            </h1>
+         }
+      </div>
    </nav>
   )
 }
